46d49a4506db3d90acf54f0bee6db7899f11ceb8
Sung-ho Lee <s1217sh@gmail.com>
2015-09-04 23:34:43 +0900

    Changed pin number to support ATTiny45/85 and some other things.

--- RandomizedLEDCube.ino
+++ RandomizedLEDCube.ino
@@ -1,28 +1,29 @@
 /**
- * 
+ *
  * Randomized LED Cube
- * 
- * Author: Sung-ho Lee (a.k.a. YameCoder) <sh1217sh@gmail.com>
- * 
+ *
+ * Author: Sung-ho Lee (a.k.a. NullPointer) <sh1217sh@gmail.com>
+ *
  * Hardware configuration : Common cathod for each layer. Any size you want as the number of Arduino pins allows.
- * 
+ *
  * TESTED WITH ARDUINO UNO AND CUSTOM MADE LED CUBE. THE AUTHOR DOES NOT GARANTEE WHETHER THIS SOFTWARE IS APPLICATABLE ON OTHER ENVIRONMENTS.
- * 
+ *
  * ***** LICENSE (IMPORTANT!) *****
- * Copyright (C) 2015 Sung-ho Lee (a.k.a. YameCoder) <sh1217sh@gmail.com>
- * 
+ * The MIT License.
+ * Copyright (C) 2015 Sung-ho Lee (a.k.a. NullPointer) <sh1217sh@gmail.com>
+ *
  * You can do whatever you want including modifying, redistributing, using commocially, etc. if you retain this license claim.
  * ********************************
- * 
+ *
  */
 
 // ==== Configuration ====
 #define NUMBER_OF_LAYERS 2
 #define LEDS_ON_A_LAYER 4
-#define DURATION 750
+#define INTERVAL 750
 
-const byte layers[NUMBER_OF_LAYERS] = {9, 8};
-const byte leds[LEDS_ON_A_LAYER] = {13, 12, 11, 10};
+const byte layers[NUMBER_OF_LAYERS] = {5, 4};
+const byte leds[LEDS_ON_A_LAYER] = {3, 2, 1, 0};
 // =======================
 
 boolean currentPattern[NUMBER_OF_LAYERS][LEDS_ON_A_LAYER];
@@ -31,9 +32,12 @@ long lastMillis = 0;
 
 void setup() {
   // Set pins to output mode and initialize.
-  for (int i = 8; i<=13; i++) {
-    pinMode(i, OUTPUT);
-    digitalWrite(i, LOW);
+  byte i;
+  for (i = 0; i < NUMBER_OF_LAYERS; i++) {
+    pinMode(layers[i], OUTPUT);
+  }
+  for (i = 0; i < LEDS_ON_A_LAYER; i++) {
+    pinMode(leds[i], OUTPUT);
   }
 
   //Serial.begin(9600);
@@ -41,7 +45,7 @@ void setup() {
 
 void loop() {
   // Change LED pattern when setted time is over.
-  if (millis() - lastMillis > DURATION) {
+  if (millis() - lastMillis > INTERVAL) {
     changePattern();
     lastMillis = millis();
   }
@@ -51,9 +55,9 @@ void loop() {
 void display() {
   // Display current pattern on the cube.
   // Uses dynamic driving
-  for (int i=0; i<NUMBER_OF_LAYERS; i++) {
+  for (byte i=0; i<NUMBER_OF_LAYERS; i++) {
     digitalWrite(layers[i], LOW);
-    for (int j=0; j<LEDS_ON_A_LAYER; j++) {
+    for (byte j=0; j<LEDS_ON_A_LAYER; j++) {
       digitalWrite(leds[j], currentPattern[i][j]);
     }
     digitalWrite(layers[i], HIGH);
@@ -64,21 +68,20 @@ void display() {
 
 void changePattern() {
   // Randomize the pattern.
-  for (int i=0; i<NUMBER_OF_LAYERS; i++) {
-    for (int j=0; j<LEDS_ON_A_LAYER; j++) {
+  for (byte i=0; i<NUMBER_OF_LAYERS; i++) {
+    for (byte j=0; j<LEDS_ON_A_LAYER; j++) {
       currentPattern[i][j] = random(0, 2);
     }
   }
 }
 
 // For debuging purpose. Call this function if you need it.
-void printCurrentPattern() {
-  for (int i=0; i<NUMBER_OF_LAYERS; i++) {
-    for (int j=0; j<LEDS_ON_A_LAYER; j++) {
-      Serial.print(currentPattern[i][j]);
-    }
-    Serial.println("");
-  }
-  Serial.println("");
-}
-
+// void printCurrentPattern() {
+//   for (byte i=0; i<NUMBER_OF_LAYERS; i++) {
+//     for (byte j=0; j<LEDS_ON_A_LAYER; j++) {
+//       Serial.print(currentPattern[i][j]);
+//     }
+//     Serial.println("");
+//   }
+//   Serial.println("");
+// }
